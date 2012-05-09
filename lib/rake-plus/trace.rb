066ac94c25c5bc09c3ddaf044df23d4b7030a593
Jonas Pfenniger <jonas@pfenniger.name>
2012-05-09 17:51:24 +0100

    Default rake-plus requires

---
+++ lib/rake-plus/trace.rb
@@ -0,0 +1,4 @@
+# Tired of re-running your rake task with -t because it throwed an exception ?
+#
+# This will turn on the trace by default so that you don't have to do it again.
+Rake.application.options.trace = true
